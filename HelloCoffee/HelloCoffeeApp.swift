2271b3fd580d6f493bb8be6350312fcf934040c1
superkookai <weerawut@live.com>
2025-03-04 11:09:49 +0700

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Weerawut Chaiyasomboon on 04/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
